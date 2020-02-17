2a30fae9516c20b19fd8dc6daa05c1b631327e05
mjarraya <mjarraya@student.42.fr>
2020-02-17 16:10:41 +0100

    bulma lab

---
+++ extra.md
@@ -0,0 +1,1 @@
+[shortcutfoo](https://www.shortcutfoo.com/)
--- react-components/src/index.js
+++ react-components/src/index.js
@@ -1,20 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const Heading = () => {
-//   return <h1>React Components</h1>;
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
+
+// const Heading = props => {
+//   return <h1>{props.children}</h1>;
 // };
 
 class Heading extends React.Component {
   render() {
-    return <h1>React Components</h1>;
+    // console.log(this.props.children); // props is the name of the property
+    return <h1>{this.props.children}</h1>;
   }
 }
 
+const Foo = props => {
+  return <span></span>;
+};
+
+const Greeting = props => {
+  return (
+    <p>
+      <Foo person={props.person} />{" "}
+      {/* props are not automatically propagated down and we have to explicitly do so */}
+      {props.greeting} {props.person?.firstName} {props.person?.lastName}
+    </p>
+  );
+};
+
 const App = () => {
+  const obj = {
+    firstName: "John",
+    lastName: "Doe"
+  };
+
   return (
     <div>
-      <Heading />
+      <Navbar />
+      <Heading>React Components</Heading>
+      <Heading>React Props</Heading>
+
+      <Greeting greeting="Hello" person={obj} />
+      <Greeting
+        greeting="Bonjour"
+        person={{
+          firstName: "Foo",
+          lastName: "Bar"
+        }}
+      />
+      <Greeting greeting="Hola" />
     </div>
   );
 };
