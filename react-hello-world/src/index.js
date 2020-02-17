17f13e4658049c8dc0be701cad529638f8b36996
mjarraya <mjarraya@student.42.fr>
2020-02-17 10:02:59 +0100

    react intro

---
+++ react-hello-world/src/index.js
@@ -0,0 +1,21 @@
+// const React = require('react')
+// const ReactDOM = require('react-dom')
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+  </div>,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(
+//   React.createElement(
+//     "div",
+//     null,
+//     React.createElement("h1", null, "Hello World 42!")
+//   ),
+//   document.getElementById("root")
+// );
